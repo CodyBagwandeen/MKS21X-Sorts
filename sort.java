41a654bc9aeaa782ba5f42917900d9fed265e2bf
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-12-04 20:06:30 -0500

    did the sort, tested it in main

--- sort.java
+++ sort.java
@@ -1,17 +1,34 @@
+import java.lang.*;
+import java.util.*;
 public class sort {
   //choose the smallest put it into ary[0]
   //choose the next smallest put it into ary[1]  (you can ignore the slots before the current position)
   //choose the next smallest put it into ary[2]  (you can ignore the slots before the current position)
   public static void selectionsort( int[] ary) {
-    int[] temp = new int[ary.length];
     for( int i = 0; i < ary.length; i++) {
       int val = 0;
-      for( int j = 0; j < ary.length; j++) {
-        if( val < ary[j])
-        val = j;
+      int index = 0;
+      int temp = 0;
+      for( int j = i; j < ary.length; j++) {
+        if( ary[j] <= val) {
+          val = ary[j];
+          index = j;
+        }
       }
-      temp[i] = val;
+      temp = ary[i];
+      ary[i] = val;
+      ary[index] = temp;
     }
-    ary = temp;
+    //System.out.println(Arrays.toString(ary));
+  }
+
+  public static void main( String[] args){
+    int[] a = {15,2,-3,189,-1231,7,5,9,7,12};
+
+    System.out.println(Arrays.toString(a));
+
+    selectionsort(a);
+
+    System.out.println(Arrays.toString(a));
   }
 }
