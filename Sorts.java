c6629e5ce9dd3ed19083e6c30ffb913ec053a3aa
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-12-08 21:19:57 -0500

    bubbleSorts works but it still checks even after its all sorted.

--- Sorts.java
+++ Sorts.java
@@ -23,10 +23,18 @@ public class Sorts {
   }
 
   public static void bubblesort( int [] ary) {
-    boolean Sorted = false;
-    while( !Sorted) {
+    //boolean Sorted = false;
+    //while( !Sorted) {
       for( int i = 0; i < ary.length; i++) {
-        
+        for ( int j = 0; j < ary.length - ( i+ 1); j++) {
+          if ( ary[j] > ary[j+1]) {
+            int a = ary[j];
+            int b = ary[j+1];
+            ary[j] = b;
+            ary[j+1] = a;
+            //Sorted = false;
+          //}
+        }
       }
     }
 
@@ -44,12 +52,29 @@ public class Sorts {
     System.out.println(Arrays.toString(c));
     System.out.println(Arrays.toString(d));
     System.out.println(Arrays.toString(e));
+    System.out.println(" \n");
 
-    selectionsort(a);
-    selectionsort(b);
-    selectionsort(c);
-    selectionsort(d);
-    selectionsort(e);
+    //selectionsort(a);
+    //selectionsort(b);
+    //selectionsort(c);
+    //selectionsort(d);
+    //selectionsort(e);
+
+
+
+    bubblesort(a);
+    bubblesort(b);
+    bubblesort(c);
+    bubblesort(d);
+    bubblesort(e);
+
+    System.out.println("A sorted : [-1231, -3, 2, 5, 7, 7, 9, 12, 15, 189]");
+    System.out.println("B sorted : [-9, -7, -1, 5, 12, 15, 19, 31, 97, 2123]");
+    System.out.println("C sorted : [-3234, -1231, -612, -189, -5, 2, 7, 9, 37, 115]");
+    System.out.println("D sorted : [-125, -7, -3, 2, 5, 7, 9, 12, 189, 1231]");
+    System.out.println("E sorted : [-3345345, -5578, -1231, -189, -7, 2, 7, 9, 12, 15]");
+    System.out.println("\n");
+
 
     System.out.println(Arrays.toString(a));
     System.out.println(Arrays.toString(b));
