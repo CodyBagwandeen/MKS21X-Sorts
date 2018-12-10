0f9e5763eb05117d14db3711b29d0ba60592d26f
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-12-10 11:45:31 -0500

    Stuff from class

--- Sorts.java
+++ Sorts.java
@@ -4,7 +4,7 @@ public class Sorts {
   //choose the smallest put it into ary[0]
   //choose the next smallest put it into ary[1]  (you can ignore the slots before the current position)
   //choose the next smallest put it into ary[2]  (you can ignore the slots before the current position)
-  public static void selectionsort( int[] ary) {
+  public static void selectionSort( int[] ary) {
     for( int i = 0; i < ary.length; i++) {
       int val = ary[i];
       int index = 0;
@@ -22,7 +22,7 @@ public class Sorts {
     //System.out.println(Arrays.toString(ary));
   }
 
-  public static void bubblesort( int [] ary) {
+  public static void bubbleSort( int [] ary) {
     boolean Sorted = false;
       for( int i = 0; i < ary.length; i++) {
         while( !Sorted) {
@@ -36,11 +36,26 @@ public class Sorts {
               Sorted = false;
           }
         }
-          System.out.println(Arrays.toString(ary));
+          //System.out.println(Arrays.toString(ary));
       }
     }
 
   }
+
+public static void insertionSort(int [] ary) {
+  for( int i = 1; i < ary.length; i++) {
+    if( ary[i] < ary[i-1]) {
+      int value = ary[i];
+      for( int j = i-1; j > i ; )
+    }
+  }
+}
+
+
+
+
+
+
 
   public static void main( String[] args){
     int[] a = {15,2,-3,189,-1231,7,5,9,7,12};
@@ -62,11 +77,11 @@ public class Sorts {
     //selectionsort(d);
     //selectionsort(e);
 
-    bubblesort(a);
-    bubblesort(b);
-    bubblesort(c);
-    bubblesort(d);
-    bubblesort(e);
+    bubbleSort(a);
+    bubbleSort(b);
+    bubbleSort(c);
+    bubbleSort(d);
+    bubbleSort(e);
 
     System.out.println("A sorted : [-1231, -3, 2, 5, 7, 7, 9, 12, 15, 189]");
     System.out.println("B sorted : [-9, -7, -1, 5, 12, 15, 19, 31, 97, 2123]");
